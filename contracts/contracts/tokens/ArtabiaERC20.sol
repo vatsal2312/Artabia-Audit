e209e5f76d5246c7b55d65614a75b9b0471aeb39
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-10-08 18:56:01 +0530

    Update ArtabiaERC20.sol

--- contracts/contracts/tokens/ArtabiaERC20.sol
+++ contracts/contracts/tokens/ArtabiaERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../ERC20.sol";
 
 contract ArtabiaERC20 is ERC20 {
   constructor() ERC20("Artabia Token", "ABA") {}
