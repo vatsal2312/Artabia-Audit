7335d06156cfc8770abc378439827d0ba4704406
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-10-08 19:00:41 +0530

    Create OnlyNonContractsOrApproved.sol

---
+++ contracts/contracts/OnlyNonContractsOrApproved.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.9;
+
+import "./Address.sol";
+import "./Ownable.sol";
+
+/**
+ * @dev Auxiliary contract that exposes a onlyNonContractsOrApproved
+ *      modifier allows only approved contracts to execute a function.
+ */
+abstract contract OnlyNonContractsOrApproved is Ownable{
+  using Address for address;
+
+  mapping(address => bool) approvedContracts;
+
+  /**
+   * @dev Adds an approved contract
+   *
+   * @param _contractAddress the address of the contract to be added
+   */
+  function addApprovedContract(address _contractAddress) external onlyOwner {
+    approvedContracts[_contractAddress] = true;
+  }
+
+  /**
+   * @dev Allows only for Externally-owned accounts (EOAs)
+          or contracts that have been approved by the owner
+   */
+  modifier onlyNonContractsOrApproved() {
+    require(!msg.sender.isContract() || approvedContracts[msg.sender], "only non contracts or approved");
+    _;
+  }
+}
